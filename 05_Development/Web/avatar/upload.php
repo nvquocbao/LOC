40fa865b1562fccd4c2207016af3cf6207756370
TrinhNT <trinhnt@evolable.asia>
2016-12-11 08:55:41 +0900

    Update

--- 05_Development/Web/avatar/upload.php
+++ 05_Development/Web/avatar/upload.php
@@ -47,7 +47,7 @@
 			 move_uploaded_file($_FILES['image']['tmp_name'],$file_path);
 			 //filling response array with values 
 			 $response['resultCode'] = 0; 
-			 $response['url'] = $file_url;
+			 $response['returnObject'] = $file_url;
 		 }catch(Exception $e){
 			 $response['resultCode']=1;
 			 $response['message']=$e->getMessage();
