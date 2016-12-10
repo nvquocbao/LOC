132264899d5b4c15f65c1614387b2b2a7b8c4dac
nvquocbao <nvquocbao@gmail.com>
2016-12-10 13:50:43 +0900

    Add model entity and data access object

---
+++ 05_Development/Web/LOCWS/src/model/dao/MessageHome.java
@@ -0,0 +1,72 @@
+package model.dao;
+
+// Generated 2016/12/10 13:14:17 by Hibernate Tools 4.3.1
+
+//import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import model.bean.Message;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Home object for domain model class Message.
+ * @see dao.Message
+ * @author Hibernate Tools
+ */
+//@Stateless
+public class MessageHome {
+
+	private static final Log log = LogFactory.getLog(MessageHome.class);
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public void persist(Message transientInstance) {
+		log.debug("persisting Message instance");
+		try {
+			entityManager.persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void remove(Message persistentInstance) {
+		log.debug("removing Message instance");
+		try {
+			entityManager.remove(persistentInstance);
+			log.debug("remove successful");
+		} catch (RuntimeException re) {
+			log.error("remove failed", re);
+			throw re;
+		}
+	}
+
+	public Message merge(Message detachedInstance) {
+		log.debug("merging Message instance");
+		try {
+			Message result = entityManager.merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Message findById(int id) {
+		log.debug("getting Message instance with id: " + id);
+		try {
+			Message instance = entityManager.find(Message.class, id);
+			log.debug("get successful");
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+}
