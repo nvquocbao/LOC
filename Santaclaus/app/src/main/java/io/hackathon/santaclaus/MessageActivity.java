649faf16848f8a470e4941c1d3183c3251813a4c
TrinhNT <trinhnt@evolable.asia>
2016-12-10 13:19:16 +0900

    Add MessageActivity

--- Santaclaus/app/src/main/java/io/hackathon/santaclaus/MessageActivity.java
+++ Santaclaus/app/src/main/java/io/hackathon/santaclaus/MessageActivity.java
@@ -9,5 +9,7 @@ public class MessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+
     }
 }
